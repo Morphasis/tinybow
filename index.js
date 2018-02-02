3387660f2738446f898679a68da0db72598dc991
Morphasis <jacksharville@gmail.com>
2018-02-02 20:30:48 +0000

    Fixed ports

--- index.js
+++ index.js
@@ -52,9 +52,8 @@ app.post('/submit-order', function (req, res) {
     }
   });
 })
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var server_port = process.env.PORT || 8080
 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+app.listen(server_port, function () {
+  console.log( "Listening on " + server_port )
 });
